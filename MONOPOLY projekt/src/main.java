a115a17eeeb8333e05dd6e968c9a6625fa1803b0
Czajsoon <kubaczajkowski25@gmail.com>
2021-02-26 09:44:24 +0100

    first window

--- MONOPOLY projekt/src/main.java
+++ MONOPOLY projekt/src/main.java
@@ -1,7 +1,18 @@
+import javax.swing.*;
+
 public class main {
     public static void main(String[] args){
-        System.out.print("Hello Monopoly");
-        System.out.print("...");
+//        System.out.print("Hello Monopoly");
+//        System.out.print("...");
+        JFrame f = new JFrame();
 
+        JButton b = new JButton("kliknij mnie!");
+        b.setBounds(100,210,200,40);
+
+        f.add(b);
+
+        f.setSize(400,500);
+        f.setLayout(null);
+        f.setVisible(true);
     }
 }
