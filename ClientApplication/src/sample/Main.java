3a0171d940625012fbb2768d8300696aa59c4752
bkprog <73074013+bkprog@users.noreply.github.com>
2021-03-04 14:35:39 +0100

    Poczekalna w fx vol 1

--- ClientApplication/src/sample/Main.java
+++ ClientApplication/src/sample/Main.java
@@ -11,12 +11,10 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Poczekalnia");
+        primaryStage.setScene(new Scene(root, 1200, 800));
         primaryStage.show();
-    }
-
-
+}
     public static void main(String[] args) {
         launch(args);
     }
