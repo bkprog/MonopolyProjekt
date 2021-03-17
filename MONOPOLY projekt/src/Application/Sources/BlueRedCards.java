9f6e4089b1b482bcb92a3aee02ccd247c6ec68b0
Czajsoon <kubaczajkowski25@gmail.com>
2021-03-17 21:13:03 +0100

    Blue and red Cards class update!

--- MONOPOLY projekt/src/Application/Sources/BlueRedCards.java
+++ MONOPOLY projekt/src/Application/Sources/BlueRedCards.java
@@ -1,18 +1,42 @@
 package Application.Sources;
 
 public class BlueRedCards {
+    private int CardId;
     private String cardText;
-    private int cash;
+    private int cashReward;
+    private int cashFine;
+    private int fieldsForward;
+    private int fieldsBackward;
     private int destinationField;
 
-    public BlueRedCards(String cardText,int cash,int destinationField){
+    public BlueRedCards(int cardId,String cardText,int cashReward,int cashFine,int fieldsForward,int fieldsBackward,int destinationField){
+        this.CardId = cardId;
         this.cardText = cardText;
-        this.cash = cash;
+        this.cashReward = cashReward;
+        this.cashFine = cashFine;
+        this.fieldsForward = fieldsForward;
+        this.fieldsBackward = fieldsBackward;
         this.destinationField = destinationField;
     }
 
-    public int getCash() {
-        return this.cash;
+    public int getCardId(){
+        return this.CardId;
+    }
+
+    public int getCashReward() {
+        return this.cashReward;
+    }
+
+    public int getCashFine(){
+        return this.cashFine;
+    }
+
+    public int getFieldsForward(){
+        return this.fieldsForward;
+    }
+
+    public int getFieldsBackward(){
+        return this.fieldsBackward;
     }
 
     public String getCardText() {
