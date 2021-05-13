f5bd7748c02ea08bd3d480942af0c02ee3885667
bkprog <73074013+bkprog@users.noreply.github.com>
2021-05-13 20:47:05 +0200

    set&get methods test

--- clientApplication/src/Test/Source/PlayerTest.java
+++ clientApplication/src/Test/Source/PlayerTest.java
@@ -10,10 +10,10 @@ public class PlayerTest {
     public void playerDefaultsettingsTest(){
         final Player playertest= new Player();
 
-        if(playertest.getIsInJail()==true){
+        if(playertest.getIsInJail()){
             throw new IllegalStateException("Player is in jail before game start");
         }
-        if(playertest.isBankroupt()==true){
+        if(playertest.isBankroupt()){
             throw new IllegalStateException("Player is bankroupt before game start");
         }
         if(playertest.getPropertyId()!=0)
@@ -21,4 +21,44 @@ public class PlayerTest {
             throw new IllegalStateException("Player PropertyID is not 0 on the start");
         }
     }
+
+    @Test
+    public void PlayerSetandgetTest(){
+        final Player playertest= new Player();
+
+        String nameTest="Janusz";
+        int TestplayerNumber=1;
+        int Testcash=1000;
+        boolean TestisInJail = false;
+        int TestpropertyId = 0;
+
+
+        playertest.setPlayerName(nameTest);
+        playertest.setPlayerNumber(TestplayerNumber);
+        playertest.setCash(Testcash);
+        playertest.setInJail(TestisInJail);
+        playertest.setPropertyId(TestpropertyId);
+        playertest.makeBankropt();
+
+        if(playertest.getPlayerName()!=nameTest){
+            throw new IllegalStateException("wrong name setting");
+        }
+        if(playertest.getPlayerNumber()!=TestplayerNumber){
+            throw new IllegalStateException("wrong player number setting");
+        }
+        if(playertest.getCash()!=Testcash){
+            throw new IllegalStateException("wrong cash setting");
+        }
+        if(playertest.getIsInJail()!=TestisInJail){
+            throw new IllegalStateException("wrong jail status setting");
+        }
+        if(playertest.getPropertyId()!=TestpropertyId){
+            throw new IllegalStateException("wrong name setting");
+        }
+        if(playertest.isBankroupt()==false){
+            throw new IllegalStateException("making bancroput doesn't work");
+        }
+    }
 }
+
+
